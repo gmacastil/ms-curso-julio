dc8f14e8d1663b8a0bb47e33c36e2fbc0ef7dc4c
Alex Mauricio Castillo <alecasti@nttdata.com>
2025-07-12 09:35:09 -0500

    add 1

---
+++ src/main/java/com/lite/ms_curso_julio/MsCursoJulioApplication.java
@@ -0,0 +1,13 @@
+package com.lite.ms_curso_julio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCursoJulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCursoJulioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lite/ms_curso_julio/MsCursoJulioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lite.ms_curso_julio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCursoJulioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
